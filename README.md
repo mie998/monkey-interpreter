8fcb3667bf359c9f30a92606b4ecb0c60bebfece
mie998 <kei.west.post920@gmail.com>
2021-10-09 16:39:20 +0900

    added some functions to lexer.go

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # monkey-interpreter
 Go言語で作るインタプリタ 実装
+
+## TODO
+- Unicode への対応(UTF-8 と UTF-16 を処理する, p7)
--- token/token.go
+++ token/token.go
@@ -1,5 +1,12 @@
 package token
 
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -9,18 +16,43 @@ const (
 	INT   = "INT"
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	// comparison operators
+	LT = "<"
+	GT = ">"
 
 	// delimter
 	COMMA     = ","
 	SEMICOLON = ";"
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
+	COLON     = ":"
+
+	// parentheses and brackets
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
 
 	// keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
