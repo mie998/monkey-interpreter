234e4e87e1e4e264eff26774c0b97d80a7e8e6e6
mie998 <kei.west.post920@gmail.com>
2021-10-09 14:51:08 +0900

    initial commit

---
+++ token/token.go
@@ -0,0 +1,26 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// identifier
+	IDENT = "IDENT"
+	INT = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// delimter
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE= "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
