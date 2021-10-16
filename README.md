567dfdbce98ab469e5299c1040e1f3c8454c34c0
mie998 <kei.west.post920@gmail.com>
2021-10-16 15:14:44 +0900

    Added some String() method for debug ast

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Go言語で作るインタプリタ 実装
 ## TODO
 - Unicode への対応(UTF-8 と UTF-16 を処理する, p7)
 - 3文字以上必要な演算子への対応 (p19 とか)
+- トップダウン式構文解析とボトムアップ構文解析との違いを理解 (p30)
+- statement を増やす (p30 ~)
+  - 今は let と return のみ
+- if を statement でなく expression にしてみる
--- ast/ast.go
+++ ast/ast.go
@@ -45,6 +45,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type ExpressionStatement struct {
 	Token      token.Token
@@ -53,6 +54,12 @@ type ExpressionStatement struct {
 
 func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
 
 type LetStatement struct {
 	Token token.Token
@@ -62,11 +69,38 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String() + " ")
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
 }
 
-func (ls *ReturnStatement) statementNode()       {}
-func (ls *ReturnStatement) TokenLiteral() string { return ls.Token.Literal }
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
