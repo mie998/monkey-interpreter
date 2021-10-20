fff3d45196544958bf8527ab1f1c8e147b5a9875
mie998 <nishiwaki.kyoto@gmail.com>
2021-10-20 16:24:59 +0900

    initiate object style of evaluation

---
+++ object/object.go
@@ -0,0 +1,31 @@
+package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOREAN_OBJ = "BOOLEAN"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOREAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
